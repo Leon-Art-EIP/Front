8a0cd1a8c527da7885885c3598d6d0d8722b86f5
Guillaume Paris <guillaume1.paris@epitech.eu>
2023-09-08 19:05:36 +0200

    replace css by tailwind and create child component for form

---
+++ src/app/forgotten_password/form.tsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+
+export interface FormProps {
+  handleSubmit: (event: React.FormEvent<HTMLFormElement>) => void;
+  error: string;
+  setError: (error: string) => void;
+}
+
+export default function Form({ handleSubmit, error, setError }: FormProps): JSX.Element {
+  
+  function handleInputChange() {
+    setError("");
+  };
+
+  return (
+    <form className="flex flex-col gap-2 w-full mt-6 xl:mt-24" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        name="email"
+        className="rounded-[30px] shadow-lg bg-[#F5F5F5] text-gray-700 py-3 px-7 w-full focus:outline-none focus:ring-1 focus:ring-[#ae1609] placeholder-gray-500"
+        placeholder="Adresse email"
+        onChange={handleInputChange}
+      />
+      <div className="relative">
+        {error && <label className="absolute top-2 text-sm font-normal text-red-500">{error}</label>}
+        <button type="submit" className="py-3 rounded-[30px] shadow-lg bg-[#E11C0A] text-white mt-10 w-full hover:bg-[#c51708] disabled:bg-gray-300" disabled={error !== ""} name="reset">
+          Réinitialiser
+        </button>
+      </div>
+      <label className="flex justify-center items-center font-normal xl:flex-row flex-col">
+        Vous vous souvenez du mot de passe ?
+        <a className="ms-1 font-extrabold text-[#E11C0A] cursor-pointer" title="login" href="/login">
+          S'identifier
+        </a>
+      </label>
+    </form>
+  );
+}
--- src/app/forgotten_password/page.tsx
+++ src/app/forgotten_password/page.tsx
@@ -1,21 +1,14 @@
 "use client";
 
-import { useRouter } from "next/navigation";
 import React, { useState } from "react";
-import { useSetRecoilState } from "recoil";
 import Gallery from "../../components/gallery";
-import { IError, ISuccess } from "../../interfaces";
-import { isLoggedIn } from "../../recoil/SetupRecoil";
-import "./page.css";
+import Form from "./form";
 
 interface IBaseFormValues {
   email: string;
 }
 
 export default function Page(): JSX.Element {
-  const [disableLogin, setDisableLogin] = useState(false);
-  const setLoggedIn = useSetRecoilState(isLoggedIn);
-  const router = useRouter();
 
   const [error, setError] = useState("");
 
@@ -24,21 +17,14 @@ export default function Page(): JSX.Element {
 
     if (!email) {
       setError("Veuillez remplir le champ.");
-      setDisableLogin(true);
       return false;
     } else if (!emailRegex.test(email)) {
       setError("Adresse email invalide.");
-      setDisableLogin(true);
       return false;
     }
-    setDisableLogin(false);
+    setError("");
     return true;
   }
-
-  const handleInputChange = () => {
-    setError("");
-    setDisableLogin(false);
-  };
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
@@ -61,37 +47,17 @@ export default function Page(): JSX.Element {
 
   return (
     <div className="flex h-screen">
-      <div className="left-pannel h-screen xl:w-1/3 w-full flex flex-col items-center justify-center fixed">
+      <div className="shadow-[10px_0_13px_-7px_rgba(170,170,170)] h-screen xl:w-1/3 w-full flex flex-col items-center justify-center fixed">
         <label className="xl:hidden block text-6xl font-bold">
           <span className="leon-title">Leon</span>
           <span className="art-title">'Art</span>
         </label>
         <div className="max-w-xs w-full pt-28 xl:pt-0">
           <label className="xl:text-5xl text-2xl xl:font-extrabold xl:leading-relaxed font-semibold w-4/6 xl:text-center text-start">Forgot your password ?</label>
-          <form className="flex flex-col gap-2 w-full mt-6 xl:mt-24" onSubmit={handleSubmit}>
-            <input
-              type="email"
-              name="email"
-              className="login-text-field"
-              placeholder="Adresse email"
-              onChange={handleInputChange}
-            />
-            <div className="relative">
-              {error && <label className="absolute top-2 text-sm font-normal text-red-500">{error}</label>}
-              <button type="submit" className="login-button mt-10 w-full" disabled={disableLogin} name="reset">
-                Réinitialiser
-              </button>
-            </div>
-            <label className="flex justify-center items-center font-normal xl:flex-row flex-col">
-              Vous vous souvenez du mot de passe ?
-              <a className="ms-1 font-extrabold" title="login" href="/login">
-                S'identifier
-              </a>
-            </label>
-          </form>
+          <Form handleSubmit={handleSubmit} error={error} setError={setError}></Form>
         </div>
       </div>
-      <div className="xl:block hidden right-side w-2/3 p-4">
+      <div className="xl:block hidden ml-[33.33%] w-2/3 p-4">
         <Gallery redirectUrl={"/login"} redirectText={"S'identifier"}></Gallery>
       </div>
     </div>
