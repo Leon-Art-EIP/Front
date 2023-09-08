7452abaeefa7f02b6be25f4e8341dee2296c3a62
MariusNWK <marius.nowak@epitech.eu>
2023-09-08 20:37:28 +0200

    wip: add unit test for single art page

---
+++ __test__/app/single.test.tsx
@@ -0,0 +1,28 @@
+import { render } from "@testing-library/react";
+import React from "react";
+import { expect, test } from "vitest";
+import SingleArtPage from "../../src/components/single-art-page/SingleArtPage";
+import data from "../../src/components/single-art-page/fakeData";
+
+test("Messages page test", () => {
+  const { container } = render(
+    <SingleArtPage
+      art={data.art}
+      artistId={data.artistId}
+      artistName={data.artistName}
+      belongingCollections={data.belongingCollections}
+      belongingCommands={data.belongingCommands}
+      caracteristics={data.caracteristics}
+      collections={data.collections}
+      description={data.description}
+      link="a"
+      price={data.price}
+      profile={data.profile}
+      title={data.title}
+      liked={data.liked}
+      nbrLikes={data.nbrLikes}
+    />
+  );
+
+  expect(container.textContent).toContain("Faire une offre");
+});
