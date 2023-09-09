710fefc0ac951d545940ed3766b6960e2c3b6c4e
Guillaume Paris <guillaume1.paris@epitech.eu>
2023-09-10 00:02:43 +0200

    replace css file by tailwind

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -3,11 +3,10 @@
 import { useRouter } from "next/navigation";
 import React, { useState } from "react";
 import { useSetRecoilState } from "recoil";
-import zxcvbn from 'zxcvbn';
+import zxcvbn from "zxcvbn";
 import Gallery from "../../components/gallery";
 import { IError, ISuccess } from "../../interfaces";
 import { isLoggedIn } from "../../recoil/SetupRecoil";
-import "./page.css";
 
 interface IBaseFormValues {
   username: string;
@@ -25,7 +24,7 @@ export default function Page(): JSX.Element {
 
   function validateForm({ username, email, password, conscent }: IBaseFormValues) {
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  
+
     if (!username || !email || !password) {
       setError("Veuillez remplir tous les champs.");
       setDisableRegister(true);
@@ -92,57 +91,64 @@ export default function Page(): JSX.Element {
 
   return (
     <div className="flex h-screen">
-      <div className="left-side h-screen w-1/3 flex flex-col items-center justify-center fixed">
-        <label className="text-5xl font-extrabold"> S'enregistrer</label>
-        <form className="flex flex-col gap-8 w-4/6" onSubmit={handleSubmit}>
-          <input
-            type="text"
-            name="username"
-            className="register-text-field"
-            placeholder="Nom d'utilisateur"
-            onChange={handleInputChange}
-          />
-          <input
-            type="email"
-            name="email"
-            className="register-text-field"
-            placeholder="Adresse email"
-            onChange={handleInputChange}
-          />
-          <input
-            type="password"
-            name="password"
-            className="register-text-field"
-            placeholder="Mot de passe"
-            onChange={handleInputChange}
-          />
-          <div className="flex flex-row">
+      <div className="shadow-[10px_0_13px_-7px_rgba(170,170,170)] h-screen xl:w-1/3 w-full flex flex-col items-center justify-center fixed">
+        <div className="max-w-xs w-full pt-28 xl:pt-0">
+          <label className="xl:text-5xl text-4xl xl:font-extrabold xl:leading-relaxed font-semibold w-full xl:text-center text-start">
+            S'enregistrer
+          </label>
+          <form className="flex flex-col gap-6 w-full mt-6 xl:mt-14" onSubmit={handleSubmit}>
             <input
-              type="checkbox"
-              name="conscent"
+              type="text"
+              name="username"
+              className="rounded-[30px] shadow-lg bg-[#F5F5F5] text-gray-700 py-3 px-7 w-full focus:outline-none focus:ring-1 focus:ring-[#ae1609] placeholder-gray-500"
+              placeholder="Nom d'utilisateur"
               onChange={handleInputChange}
             />
-            <label htmlFor="terms" className="text-sm font-normal w-11/12 text-center">
-              En vous enregistrant, vous acceptez les{" "}
-              <a className="font-semibold">Conditions d'utilisations</a> et{" "}
-              <a className="font-semibold">notre Politique de confidentialité</a>
+            <input
+              type="email"
+              name="email"
+              className="rounded-[30px] shadow-lg bg-[#F5F5F5] text-gray-700 py-3 px-7 w-full focus:outline-none focus:ring-1 focus:ring-[#ae1609] placeholder-gray-500"
+              placeholder="Adresse email"
+              onChange={handleInputChange}
+            />
+            <input
+              type="password"
+              name="password"
+              className="rounded-[30px] shadow-lg bg-[#F5F5F5] text-gray-700 py-3 px-7 w-full focus:outline-none focus:ring-1 focus:ring-[#ae1609] placeholder-gray-500"
+              placeholder="Mot de passe"
+              onChange={handleInputChange}
+            />
+            <div className="flex flex-row">
+              <input type="checkbox" name="conscent" onChange={handleInputChange} />
+              <label htmlFor="terms" className="text-sm font-normal w-11/12 text-center">
+                En vous enregistrant, vous acceptez les{" "}
+                <a className="font-semibold text-[#E11C0A] cursor-pointer">Conditions d'utilisations</a> et{" "}
+                <a className="font-semibold text-[#E11C0A] cursor-pointer">notre Politique de confidentialité</a>
+              </label>
+            </div>
+            <div className="relative">
+              {error && (
+                <label className="absolute top-2 text-sm font-normal text-red-500 error-message">{error}</label>
+              )}
+              <button
+                type="submit"
+                className="py-3 rounded-[30px] shadow-lg bg-[#E11C0A] text-white w-full hover:bg-[#c51708] disabled:bg-gray-300"
+                disabled={disableRegister}
+                name="reset"
+              >
+                S'inscrire
+              </button>
+            </div>
+            <label className="flex justify-center font-normal">
+              Vous avez déjà un compte ?{" "}
+              <a className="ms-1 font-extrabold text-[#E11C0A] cursor-pointer" title="login" href="/login">
+                Se connecter
+              </a>
             </label>
-          </div>
-          <div className="relative flex justify-center">
-            {error && <label className="absolute top-2 text-sm font-normal text-red-500">{error}</label>}
-            <button type="submit" className="register-button mt-10" disabled={disableRegister} name="register">
-              S'inscrire
-            </button>
-          </div>
-          <label className="flex justify-center font-normal">
-            Vous avez déjà un compte ?{" "}
-            <a className="ms-1 font-extrabold" title="login" href="/login">
-              Se connecter
-            </a>
-          </label>
-        </form>
+          </form>
+        </div>
       </div>
-      <div className="right-side w-2/3 p-4">
+      <div className="xl:block hidden ml-[33.33%] w-2/3 p-4">
         <Gallery redirectUrl={"/login"} redirectText={"Se connecter"}></Gallery>
       </div>
     </div>
