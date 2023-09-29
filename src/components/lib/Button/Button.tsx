081b4208f007414b5231832b9e400bf4ba92a7a1
Guillaume Paris <guillaume1.paris@epitech.eu>
2023-09-30 00:17:44 +0200

    LEON-151 add more args and options on button and input

--- src/components/lib/Button/Button.tsx
+++ src/components/lib/Button/Button.tsx
@@ -1,41 +1,30 @@
 import React from 'react';
 
+export type ButtonType = "submit" | "button";
+export type ButtonColor = "primary" | "secondary";
+
 export interface ButtonProps {
-    onClick: () => void;
-    children: React.ReactNode;
-    color?: 'danger' | 'success' | 'info';
+  children: React.ReactNode;
+  type: ButtonType;
+  color: ButtonColor;
+  className?: string;
+  onClick?: () => void;
 }
 
-const Button: React.FC<ButtonProps> = ({ onClick, children, color }) => {
-    let buttonColor: string;
-    let hoverColor: string;
-
-    switch(color) {
-        case 'danger':
-            buttonColor = 'bg-red-600 text-white';
-            hoverColor = 'hover:bg-red-700';
-            break;
-        case 'success':
-            buttonColor = 'bg-gray-200 text-purple-800';
-            hoverColor = 'hover:bg-gray-300';
-            break;
-        case 'info':
-            buttonColor = 'bg-purple-800 text-white';
-            hoverColor = 'hover:bg-purple-900';
-            break;
-        default:
-            buttonColor = '';
-            hoverColor = '';
-    }
-
-    return (
-        <button 
-            className={`rounded-lg py-3 px-10 text-base font-semibold cursor-pointer transition-colors duration-300 ease-in-out ${buttonColor} ${hoverColor}`} 
-            onClick={onClick}
-        >
-            {children}
-        </button>
-    );
+const colorClasses: Record<ButtonColor, string> = {
+  primary: "text-white bg-[#E03915] hover:bg-[#ca3313] disabled:bg-[#eb9785]",
+  secondary: "text-[#2d142c] bg-[#EEEEEE] hover:bg-[#e2e2e2] disabled:text-[#c7c2c7]",
 };
 
+const Button: React.FC<ButtonProps> = ({ children, type, color, className = "", onClick }) => (
+  <button
+    type={type}
+    className={`rounded-[20px] px-16 py-3 font-semibold text-base transition duration-200 ${colorClasses[color]} ${className}`}
+    onClick={onClick}
+  >
+    {children}
+  </button>
+);
+
 export default Button;
+
