dbb63ebaa2a5bc1f5e7aef36845ee9cfeaaebeab
Guillaume Paris <72014447+guillaume-paris@users.noreply.github.com>
2024-01-23 15:40:50 +0100

    [FIX] adding the backend url in nextconfig

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 const nextConfig = {
   env: {
-    NEXT_PUBLIC_BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL,
+    NEXT_PUBLIC_BACKEND_URL: "https://back-dev.leonart-dev.ovh/",
   },
   images: {
-    domains: ["localhost"],
+    domains: ["localhost", "front-dev.leonart-dev.ovh"],
   },
 };
 
