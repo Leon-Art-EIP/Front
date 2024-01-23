51fea771713e9a327742fb5cca2b178d1aab9de4
Guillaume Paris <72014447+guillaume-paris@users.noreply.github.com>
2024-01-23 19:23:05 +0100

    [FIX] trying to add different url path for <Image> nextjs error

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
     NEXT_PUBLIC_BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL,
   },
   images: {
-    domains: ["localhost", "front-dev.leonart-dev.ovh"],
+    domains: ["localhost", "front-dev.leonart-dev.ovh", "front-dev.leonart-dev", "front-dev", "leonart-dev"],
   },
 };
 
