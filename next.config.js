f7a4e00d211e0f83a96886c650c694b6bdafe5dd
MariusNWK <marius.nowak@epitech.eu>
2024-01-23 22:28:06 +0100

    feat: add back-dev.leonart-dev.ovh to images domains inside next.config.js

--- next.config.js
+++ next.config.js
@@ -3,7 +3,16 @@ const nextConfig = {
     NEXT_PUBLIC_BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL,
   },
   images: {
-    domains: ["localhost", "front-dev.leonart-dev.ovh", "front-dev.leonart-dev", "front-dev", "leonart-dev"],
+    domains: [
+      "localhost",
+      "front-dev.leonart-dev.ovh",
+      "front-dev.leonart-dev",
+      "front-dev",
+      "leonart-dev",
+      "back-dev.leonart-dev.ovh",
+      "back-dev.leonart-dev",
+      "back-dev",
+    ],
   },
 };
 
