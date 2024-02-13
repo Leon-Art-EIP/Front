8fa3c9d9694e6258b5e1a2541e7eb66fa55171e3
guillaume-paris <guillaume1.paris@epitech.eu>
2024-02-13 18:43:46 +0100

    [ADD] title of art publications when hover it

--- src/components/explorer/UsersSlider.tsx
+++ src/components/explorer/UsersSlider.tsx
@@ -19,14 +19,14 @@ export default function UsersSlider(props: UsersSliderProps): JSX.Element {
       {props.users.users.map((user, index) => (
         <div
           key={index}
-          className="flex flex-col cursor-pointer rounded-xl hover:opacity-75 transition-opacity hover:bg-gray-200 hover:shadow-lg duration-300 ease-in-out px-4 py-2"
+          className="flex flex-col group cursor-pointer rounded-xl hover:opacity-75 transition-opacity hover:bg-gray-200 hover:shadow-lg duration-300 ease-in-out px-4 py-2"
           onClick={() => handleClickOnUser(index)}
         >
           <div className="w-32 h-32 rounded-full overflow-hidden bg-slate-300">
             {/* eslint-disable-next-line @next/next/no-img-element */}
             <img src={`${NEXT_PUBLIC_BACKEND_URL}/api/${user.profilePicture}`} alt="art" className="object-cover" />
           </div>
-          <span className="text-xl font-semibold text-center">{user.username}</span>
+          <span className="text-xl font-semibold text-center group-hover:opacity-100">{user.username}</span>
         </div>
       ))}
     </div>
