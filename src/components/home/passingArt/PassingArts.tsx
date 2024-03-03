c01f2f4ce2135847c5966d731eff84434a72dfe6
MariusNWK <marius.nowak@epitech.eu>
2024-03-04 00:09:25 +0100

    fix: build

--- src/components/home/passingArt/PassingArts.tsx
+++ src/components/home/passingArt/PassingArts.tsx
@@ -12,7 +12,7 @@ export default function PassingArts(props: IPassingArtsProps): JSX.Element {
   const [position, setPosition] = useState(1);
 
   if (props.passingArts.length === 0) {
-    return <span className="text-lg font-medium italic">Aucun article n'est disponible</span>;
+    return <span className="text-lg font-medium italic">Aucun article n&apos;est disponible</span>;
   }
 
   const increasePosition = () => {
