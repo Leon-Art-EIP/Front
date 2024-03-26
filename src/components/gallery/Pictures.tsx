d9aee5c86a924f59841f9ed411131f0bf6f94def
Vivant-Garrigues <72013869+Vivant-Garrigues@users.noreply.github.com>
2024-03-27 02:53:44 +0900

    [ADD] all pages covered (i think)

--- src/components/gallery/Pictures.tsx
+++ src/components/gallery/Pictures.tsx
@@ -6,7 +6,7 @@ interface IPicturesProps {
 
 export default function Pictures(props: IPicturesProps): JSX.Element {
   return (
-    <div className="grid grid-cols-4 gap-8 place-items-center justify-center py-4 px-4">
+    <div className="bg-background grid grid-cols-4 gap-8 place-items-center justify-center py-4 px-4">
       {props.pictures.map((url, index) => (
         <div key={index} className="w-55 h-55 rounded-xl overflow-hidden bg-secondary">
           {/* eslint-disable-next-line @next/next/no-img-element */}
