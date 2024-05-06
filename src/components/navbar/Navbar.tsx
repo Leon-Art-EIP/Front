ec83c26a313c549b4432df2c904a4e6e4cdf116c
guillaume-paris <guillaume1.paris@epitech.eu>
2024-05-06 14:04:41 +0200

    [FIX] correctify wrong conflict merge

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -2,7 +2,6 @@ import { Settings } from "@mui/icons-material";
 import { ElementType } from "react";
 import { ITab } from "../../interfaces";
 import ProfilePicture from "../profile/profilePicture/ProfilePicture";
-import { Settings } from "@mui/icons-material";
 import Notifications from "./notifications/Notifications";
 import TextLogo from "../text-logo/TextLogo";
 
@@ -36,6 +35,7 @@ export default function Navbar({ link: Link, ...props }: INavbarProps) {
       </div>
       <div className="items-center justify-end flex px-2">
         <div className="flex gap-2 align-middle items-center text-tertiary">
+          <Notifications />
           <Link href={`/settings`}>
             <Settings />
           </Link>
