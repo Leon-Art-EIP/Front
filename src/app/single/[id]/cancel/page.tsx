2e4de7db88ef0cd9e5fa668b6e635d2eee0369d4
guillaume-paris <guillaume1.paris@epitech.eu>
2024-05-28 16:57:33 +0200

    [FIX] remove verification of length of the id when successfully created an order

--- src/app/single/[id]/cancel/page.tsx
+++ src/app/single/[id]/cancel/page.tsx
@@ -2,9 +2,5 @@ import SingleArtPageWrapper from "../../../../wrappers/SingleArtPageWrapper";
 import NotFound from "../../../not-found";
 
 export default async function Page(props: { params: { id: string } }) {
-  if (props.params.id.length !== 24) {
-    return <NotFound />;
-  }
-
   return <SingleArtPageWrapper id={props.params.id} success={false} cancel={true} />;
 }
