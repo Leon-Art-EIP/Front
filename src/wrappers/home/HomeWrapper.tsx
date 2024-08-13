ac7e354fa6f6a655f567a699b691f369f1bcdb9e
Guillaume Paris <guillaume1.paris@epitech.eu>
2024-08-13 16:47:49 +0200

    [ADD] footer

--- src/wrappers/home/HomeWrapper.tsx
+++ src/wrappers/home/HomeWrapper.tsx
@@ -4,13 +4,13 @@ import { useState } from "react";
 import UsersSlider from "../../components/explorer/UsersSlider";
 import Fetcher from "../../components/fetch/Fetcher";
 import Gallery4x4 from "../../components/gallery/Gallery4x4";
-import Articles from "../../components/home/Articles";
 import PassingArts from "../../components/home/passingArt/PassingArts";
 import { IArtPublication } from "../../interfaces/artPublication/artPublication";
 import { IUser, IUsers } from "../../interfaces/explorer/users";
 import { IArtPublications } from "../../interfaces/gallery/artPublications";
 import { IArticle } from "../../interfaces/home/article";
 import { IPassingArt } from "../../interfaces/home/passingArt";
+import Footer from "../../components/home/Footer";
 
 export default function HomeWrapper(): JSX.Element {
   const [passingArts, setPassingArts] = useState<IPassingArt[]>([]);
@@ -44,11 +44,11 @@ export default function HomeWrapper(): JSX.Element {
       <Fetcher method="GET" nbFetchs={1} route="/api/article/latest" handleOk={fetchArticles} />
       <Fetcher method="GET" nbFetchs={1} route="/api/artists/latest" handleOk={handleArtistsOk} />
       <Fetcher method="GET" nbFetchs={1} route="/api/art-publication/feed/latest" handleOk={handleArtsOk} />
-      <div className="bg-background flex flex-col items-center">
+      <div className="bg-background flex flex-col items-center h-full">
         <div className="w-full">
           <PassingArts passingArts={passingArts} />
         </div>
-        <div className="flex flex-col max-w-[1500px] w-full items-center gap-14 lg:py-8 py-4 lg:px-10 px-6">
+        <div className="flex flex-col max-w-[1500px] w-full h-full items-center gap-14 lg:py-8 py-4 lg:px-10 px-6">
           <div className="flex flex-col self-start gap-6 w-full">
             <h1 className="text-tertiary">Artistes</h1>
             <UsersSlider users={artists} />
@@ -57,9 +57,9 @@ export default function HomeWrapper(): JSX.Element {
             <h1 className="text-tertiary">Pour vous</h1>
             <Gallery4x4 artPublications={arts} />
           </div>
-          <footer className="w-full flex justify-center py-8 bg-secondary">
-            <Articles />
-          </footer>
+        </div>
+        <div className="w-full">
+          <Footer></Footer>
         </div>
       </div>
     </>
