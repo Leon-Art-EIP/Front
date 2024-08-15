d53ffb8cd1e33601df7c45de3e92b86d8e2d3e72
Guillaume Paris <guillaume1.paris@epitech.eu>
2024-08-15 16:55:43 +0200

    [FIX] 404 page design

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,30 +3,28 @@
 import { useRouter } from "next/navigation";
 import DisconnectButton from "../components/buttons/DisconnectButton";
 import { Button } from "../components/lib";
+import TextLogo from "../components/text-logo/TextLogo";
 
 export default function NotFound(): JSX.Element {
   const router = useRouter();
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <div className="flex flex-col gap-8 rounded p-8 bg-white">
-        <div className="flex flex-col gap-4">
-          <h1 className="text-4xl font-bold text-center">404 - Page introuvable</h1>
-          <div className="text-2xl text-cyan-600 rounded text-center">
-            Oh oh... Il semble que la page à laquelle vous essayez d&apos;accéder n&apos;existe pas
-          </div>
-          <div className="text-2xl text-cyan-600 rounded text-center">
-            Mais pas de panique, vous pouvez revenir en lieu sûr
+    <div className="flex items-center justify-center min-h-screen">
+      <div className="flex flex-col gap-8 rounded p-12">
+        <div className="flex flex-col items-center gap-8">
+          <span className="font-bold text-6xl cursor-default select-none pb-14">
+            <TextLogo />
+          </span>
+          <h1 className="text-4xl font-bold text-center">Page introuvable</h1>
+          <div className="text-xl text-center">
+            Oh oh... Il semble que la page à laquelle vous essayez d&apos;accéder n&apos;existe pas.<br></br>
+            Mais pas de panique, vous pouvez revenir en lieu sûr !
           </div>
         </div>
-        <div className="flex flex-col gap-2 self-center">
-          <Button onClick={() => router.push("/")} type="button" color="info">
-            Accueil
-          </Button>
-          <Button onClick={() => router.back()} type="button" color="success">
+        <div className="flex flex-row justify-center">
+          <Button onClick={() => router.back()} type="button" color="secondary">
             Retour
           </Button>
-          <DisconnectButton />
         </div>
       </div>
     </div>
