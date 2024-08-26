9e84077bacfe7f64907fe2f5422d5b9c2c6fd4e2
Guillaume Paris <guillaume1.paris@epitech.eu>
2024-08-26 18:18:08 +0200

    [ADD] taking login url's into account from google aouth redirection

--- src/components/session/index.tsx
+++ src/components/session/index.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { usePathname, useRouter } from "next/navigation";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
 import useFcmToken from "../../hooks/useFcmToken";
 import { ITab } from "../../interfaces";
@@ -19,28 +19,56 @@ export default function Session(props: ISessionProps): JSX.Element | null {
   const [user, setUser] = useState<IConnectedUser | null | undefined>(undefined);
   const pathname = usePathname();
   const router = useRouter();
+  const searchParams = useSearchParams();
   const correspondingTab = props.tabs.find((tab) => {
     if (tab.href === "/") return tab.href === pathname;
     return pathname.includes(tab.href);
   });
 
   useEffect(() => {
-    // no choice to use useEffect for localStorage
-    const local = localStorage.getItem("user");
-    if (local) {
-      const loggedInUser = JSON.parse(local);
+    const token = searchParams.get("token");
+    const username = searchParams.get("username");
+    const email = searchParams.get("email");
+    const id = searchParams.get("id");
+
+    if (token && username && email && id) {
+      const loggedInUser: IConnectedUser = {
+        token,
+        user: {
+          id: id,
+          username: username,
+          email: email,
+          is_artist: false,
+          availability: "",
+          subscription: "",
+          collections: []
+        }
+      };
+      localStorage.setItem("user", JSON.stringify(loggedInUser));
       setUser(loggedInUser);
 
-      if (loggedInUser && loggedInUser.token && fcmToken) {
+      if (fcmToken) {
         updateFcmTokenForUser(fcmToken);
       }
+      // Redirect to the home page because this statement means that the user is logged in via google aouth2 redirection
+      router.replace("/");
     } else {
-      setUser(null);
+      console.log("No token found in search params");
+      const local = localStorage.getItem("user");
+      if (local) {
+        const loggedInUser = JSON.parse(local);
+        setUser(loggedInUser);
+
+        if (loggedInUser && loggedInUser.token && fcmToken) {
+          updateFcmTokenForUser(fcmToken);
+        }
+      } else {
+        setUser(null);
+      }
     }
-  }, [pathname, fcmToken]);
+  }, [pathname, searchParams, fcmToken]);
 
   useEffect(() => {
-    // Redirect only after the initial render
     if (correspondingTab?.loggedIn !== false && (user === null || (user && !user.token))) {
       router.replace("/login");
     }
