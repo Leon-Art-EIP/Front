4778a17bd18c460d3ac041a59415d5c8280b84fe
MariusNWK <marius.nowak@epitech.eu>
2024-08-27 20:32:18 +0200

    [WIP] refacto posts page

--- src/app/posts/latest/page.tsx
+++ src/app/posts/latest/page.tsx
@@ -1,6 +1,3 @@
-import Posts from "../../../components/posts/Posts";
-import Latest from "../../../assets/new.png";
-
 export default function Page(): JSX.Element {
-  return <Posts activeTab="latest" src={Latest as unknown as { src: string }} />;
+  return <div>latest</div>
 }
--- src/app/posts/me/page.tsx
+++ src/app/posts/me/page.tsx
@@ -1,3 +1,3 @@
 export default function Page(): JSX.Element {
-  return <div>trends</div>
+  return <div>my posts</div>
 }
