26fc5934c898ee43396bb3200ec071bee5b9a044
Guillaume Paris <guillaume1.paris@epitech.eu>
2024-09-07 19:35:52 +0200

    [ADD] limit of 5MB to the image imported and error message

--- src/forms/tsx/CreateArtForm.tsx
+++ src/forms/tsx/CreateArtForm.tsx
@@ -1,14 +1,12 @@
 "use client";
 
 import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { FormProvider } from "react-hook-form";
 import Fetcher from "../../components/fetch/Fetcher";
 import Checkbox from "../../components/form/Checkbox";
 import FileInput from "../../components/form/FileInput";
 import Input from "../../components/form/Input";
-import NumberInput from "../../components/form/NumberInput";
-import Select from "../../components/form/Select";
 import TextArea from "../../components/form/TextArea";
 import { Button } from "../../components/lib";
 import { IError, IOption, IOptionSubOptions } from "../../interfaces";
@@ -16,6 +14,10 @@ import { appendFormData } from "../../tools/formData";
 import { TCreateArtData } from "../../zod";
 import useCreateArtForm from "../methods/useCreateArtForm";
 import SelectSubOptions from "../../components/form/SelectSubOptions";
+import Lightbox from "yet-another-react-lightbox";
+import Fullscreen from "yet-another-react-lightbox/plugins/fullscreen";
+import Zoom from "yet-another-react-lightbox/plugins/zoom";
+import "yet-another-react-lightbox/styles.css";
 import Link from "next/link";
 
 export interface ICreateArtFormProps {
@@ -30,6 +32,7 @@ export default function CreateArtForm(props: ICreateArtFormProps): JSX.Element {
   const router = useRouter();
   const [nbFetchs, setNbFetchs] = useState(0);
   const [body, setBody] = useState<FormData>();
+  const [isImageLightboxOpen, setImageLightboxOpen] = useState(false);
 
   const handleSubmit = async (zodData: TCreateArtData): Promise<void> => {
     const formData = new FormData();
@@ -72,6 +75,14 @@ export default function CreateArtForm(props: ICreateArtFormProps): JSX.Element {
     router.back();
   }
 
+  function onCloseLightbox() {
+    setImageLightboxOpen(false);
+  }
+
+  function onOpenLightbox() {
+    setImageLightboxOpen(true);
+  }
+
   return (
     <>
       <Fetcher method="POST" route="/api/art-publication" nbFetchs={nbFetchs} body={body} handleOk={handleOk} />
@@ -84,9 +95,35 @@ export default function CreateArtForm(props: ICreateArtFormProps): JSX.Element {
             </h2>
             <label className="text-tertiary-hover px-6">La taille de l{"'"}image ne doit pas dépasser les 5MB.</label>
             {imageImported && (
-              <img src={URL.createObjectURL(imageImported)} alt="image" className="w-full h-fit object-cover rounded" />
+              <>
+                <img
+                  src={URL.createObjectURL(imageImported)}
+                  alt=""
+                  className="w-full cursor-zoom-in rounded-2xl"
+                  onClick={onOpenLightbox}
+                />
+                <Lightbox
+                  open={isImageLightboxOpen}
+                  close={onCloseLightbox}
+                  plugins={[Zoom, Fullscreen]}
+                  zoom={{
+                    maxZoomPixelRatio: 3,
+                  }}
+                  slides={[
+                    {
+                      src: URL.createObjectURL(imageImported),
+                      alt: "image",
+                    },
+                  ]}
+                  // used to hide the navigation buttons when there is only one slide
+                  render={{
+                    iconPrev: () => <></>,
+                    iconNext: () => <></>,
+                  }}
+                />
+              </>
             )}
-            <FileInput name="image" />
+            <FileInput name="image" maxFileSize={5} /> {/* 5MB */}
           </div>
           <div className="flex flex-col gap-4">
             <h2>
@@ -107,7 +144,8 @@ export default function CreateArtForm(props: ICreateArtFormProps): JSX.Element {
             </h2>
             <div className="flex flex-col gap-6">
               <span className="text-lg italic">
-                Information importante: pour l{"'"}instant notre application est en phase Bêta. Les paiements sont donc factices.
+                Information importante: pour l{"'"}instant notre application est en phase Bêta. Les paiements sont donc
+                factices.
               </span>
               <div className="px-4">
                 <Checkbox name="isForSale" title="À vendre" />
@@ -135,70 +173,10 @@ export default function CreateArtForm(props: ICreateArtFormProps): JSX.Element {
             <Button color="secondary" type="button" onClick={handleCancel}>
               Annuler
             </Button>
-            <Button
-              color="danger"
-              type="submit"
-              disabled={isForSale && !props.stripeAccountLinked}
-            >
+            <Button color="danger" type="submit" disabled={isForSale && !props.stripeAccountLinked}>
               Publier
             </Button>
           </div>
-          {/* <Fetcher method="POST" route="/api/art-publication" nbFetchs={nbFetchs} body={body} handleOk={handleOk} />
-      <Fetcher
-        route={"/api/stripe/account-link-status"}
-        method="GET"
-        nbFetchs={nbFetchsStripeAccountAlreadyLinked}
-        handleOk={handleStipeAccountAlreadyLinked}
-      />
-      <FormProvider {...methods}>
-        <form
-          className="flex flex-col gap-8 py-12 px-16 border-x-2 border-x-gray-400 bg-white h-full"
-          onSubmit={methods.handleSubmit(onSubmit)}
-        >
-          <div className="text-2xl font-semibold">Publier</div>
-          <div className="sm:grid sm:grid-cols-3 gap-12 flex flex-col flex-1">
-            <FileInput name="image" />
-            <Select
-              name="artType"
-              title="Type d'art"
-              options={props.artTypes}
-              placeholder="Sélectionner le type d'œuvre d'art"
-              className="col-span-2"
-            />
-          </div>
-          <div className="flex gap-12 [&>*]:flex-1 sm:flex-wrap sm:flex-row flex-col">
-            <Input name="name" placeholder="Titre" type="text" className="bg-secondary p-2 rounded" />
-            <div className="border border-gray-400 h-0" />
-          </div>
-          <div className="flex gap-12 [&>*]:flex-1 sm:flex-wrap sm:flex-row flex-col flex-1">
-            <TextArea name="description" placeholder="Description" className="flex-1" />
-            <div className="flex flex-col gap-4">
-              <div className="flex flex-wrap gap-8 items-center">
-                <Checkbox name="isForSale" title="A vendre" />
-                {isForSale &&
-                  (stripeAccountAlreadyLinked ? (
-                    <NumberInput title="Prix (€)" name="price" className="bg-secondary p-2 rounded" />
-                  ) : (
-                    <div className="flex flex-col gap-4">
-                      <span className="font-semibold text-lg text-tertiary">
-                        Vous devez lier votre compte Stripe pour vendre une œuvre d&apos;art.
-                      </span>
-                      <span className="font-normal text-lg text-tertiary">
-                        Pour cela, cliquez sur le bouton ci-dessous.
-                      </span>
-                      <Button color="primary" type="button" className="w-fit" onClick={goToPersonalInformation}>
-                        Aller à mes informations personnelles
-                      </Button>
-                    </div>
-                  ))}
-              </div>
-            </div>
-          </div>
-          <Button color="danger" type="submit" className="self-end">
-            Publier
-          </Button>
-        </form>
-      </FormProvider> */}
         </form>
       </FormProvider>
     </>
