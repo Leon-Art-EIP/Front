a4576fe19d6074478c25e6dadb98953bfa49088a
Vivant-Garrigues <72013869+Vivant-Garrigues@users.noreply.github.com>
2024-09-09 19:55:29 +0200

    [ADD] mute button for chats (functional)

--- src/components/chat/chats/ChatUserCard.tsx
+++ src/components/chat/chats/ChatUserCard.tsx
@@ -1,6 +1,9 @@
 import { IChat } from "../../../interfaces/chat/chats";
 import { IConnectedUser } from "../../../interfaces/user/user";
 import { NEXT_PUBLIC_BACKEND_URL } from "../../../tools/myFetch";
+import MuteIcon from "@mui/icons-material/Notifications";
+import UnmuteIcon from "@mui/icons-material/NotificationsOff";
+import { useState } from "react";
 
 export interface ChatUserCardProps {
   chat: IChat;
@@ -11,19 +14,40 @@ export interface ChatUserCardProps {
 
 export function ChatUserCard(props: ChatUserCardProps): JSX.Element {
   /* c8 ignore start */
+
+  const [isMuted, setIsMuted] = useState<boolean>(() => {
+    const mutedChats = JSON.parse(localStorage.getItem("mutedChats") || "{}");
+    return mutedChats[props.chat._id] || false;
+  });
+
   function onSelectChat() {
     props.handleSelectChat(props.chat);
+  }
+
+  function handleMuteToggle() {
+    // Toggle mute state
+    setIsMuted((prev) => {
+      const newMutedState = !prev;
+
+      // Update localStorage
+      const mutedChats = JSON.parse(localStorage.getItem("mutedChats") || "{}");
+      mutedChats[props.chat._id] = newMutedState;
+      localStorage.setItem("mutedChats", JSON.stringify(mutedChats));
+
+      console.log("Mute/Unmute toggled", newMutedState);
+      return newMutedState;
+    });
   }
 
   return (
     <div className="pl-6 sm:pl-10">
       <div
-        className={`relative flex flex-row flex-grow-0 w-full p-2 cursor-pointer rounded-l-full hover:bg-secondary ${
+        className={`relative flex flex-row flex-grow-0 w-full p-2 cursor-pointer rounded-l-full hover:bg-secondary group ${
           props.chat._id === props.currentSelected?._id ? "bg-secondary-hover" : ""
         }`}
         onClick={onSelectChat}
       >
-        {props.chat.unreadMessages && (
+        {props.chat.unreadMessages && !isMuted && (
           <div className="absolute rounded-full w-3 h-3 bg-primary -left-4 top-1/2 transform -translate-y-1/2" />
         )}
         {(
@@ -55,6 +79,15 @@ export function ChatUserCard(props: ChatUserCardProps): JSX.Element {
             )}
           </div>
         </div>
+        <button
+          onClick={(e) => {
+            e.stopPropagation();
+            handleMuteToggle();
+          }}
+          className="absolute right-4 top-1/2 transform -translate-y-1/2 p-1 rounded-full bg-hover hidden group-hover:block"
+        >
+          {isMuted ? <UnmuteIcon /> : <MuteIcon />}
+        </button>
         {props.chat._id === props.currentSelected?._id && (
           <div className="absolute right-0 top-0 w-1 h-full bg-primary" />
         )}
