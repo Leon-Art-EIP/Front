82118fc051cf1fc2fae72acf3fbbf079df940bc9
MariusNWK <marius.nowak@epitech.eu>
2024-11-22 11:43:19 +0100

    [FIX] build

--- src/wrappers/mobile/PaymentSuccessWrapper.tsx
+++ src/wrappers/mobile/PaymentSuccessWrapper.tsx
@@ -7,14 +7,9 @@ export default function PaymentSuccessWrapper(): JSX.Element {
 
   return (
     <div className="bg-slate-50 py-4 px-6 flex flex-col items-center w-[90%] m-auto shadow-md rounded-2xl">
-      <span className="text-black mb-4">
-        Le paiement est réussi !
-      </span>
-      <button
-        onClick={handleClick}
-        className="bg-primary text-white py-2 px-6 text-lg cursor-pointer rounded-lg"
-      >
-        Retourner à l'application
+      <span className="text-black mb-4">Le paiement est réussi !</span>
+      <button onClick={handleClick} className="bg-primary text-white py-2 px-6 text-lg cursor-pointer rounded-lg">
+        Retourner à l&apos;application
       </button>
     </div>
   );
--- src/wrappers/mobile/StripeLinkedWrapper.tsx
+++ src/wrappers/mobile/StripeLinkedWrapper.tsx
@@ -7,14 +7,9 @@ export default function StripeLinkedWrapper(): JSX.Element {
 
   return (
     <div className="bg-slate-50 py-4 px-6 flex flex-col items-center w-[90%] m-auto shadow-md rounded-2xl">
-      <span className="text-black mb-4">
-        Votre compte de paiement a été lié avec votre application Leon'art !
-      </span>
-      <button
-        onClick={handleClick}
-        className="bg-primary text-white py-2 px-6 text-lg cursor-pointer rounded-lg"
-      >
-        Retourner à l'application
+      <span className="text-black mb-4">Votre compte de paiement a été lié avec votre application Leon&apos;art !</span>
+      <button onClick={handleClick} className="bg-primary text-white py-2 px-6 text-lg cursor-pointer rounded-lg">
+        Retourner à l&apos;application
       </button>
     </div>
   );
